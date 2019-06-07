76211ef76e4ee52f116d8ab56e67df51f937bf19
davidenoma <davidenoma@gmail.com>
2019-06-07 17:51:43 +0100

    first update

--- src/main/java/com/activeedge/test/SimpleJavaProgram.java
+++ src/main/java/com/activeedge/test/SimpleJavaProgram.java
@@ -3,12 +3,21 @@ package com.activeedge.test;
 public class SimpleJavaProgram {
 
     int getNonOccuring( int [] fromArray){
+        int test = fromArray.length;
 
+        for (int i = 0; i < test; i++) {
+            if (fromArray[i] < fromArray[i++]){
+                fromArray[i] = fromArray[i++];
+            }
+
+        }
 
         return 0;
     }
 
+
     public static void main(String[] args) {
+        int x = {1,2,3,4,5};
 
     }
 }
