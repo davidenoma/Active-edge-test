1605d079544b2087148aadbe5825045d07ddd2c6
davidenoma <davidenoma@gmail.com>
2019-06-07 18:50:56 +0100

    first update

--- src/main/java/com/activeedge/test/SimpleJavaProgram.java
+++ src/main/java/com/activeedge/test/SimpleJavaProgram.java
@@ -19,8 +19,33 @@ public class SimpleJavaProgram {
         }
 
 
-    }
 
+
+    }
+    private static int findLeastNonOccuring(int [] sortedArray){
+      int i = 0;
+        int diff = 0;
+        int result =  0;
+        for( ; i < sortedArray.length; i++) {
+
+            while (i + 1 < sortedArray.length){
+                 diff = sortedArray[i + 1 ] - sortedArray[i];
+            }
+
+            if(diff == 1 && diff != 0){
+                continue;
+            }
+            else {
+               result = result + 1;
+
+           }
+
+
+        }
+        if (result < 0 ) return 1;
+
+        return result;
+    }
     public static void main(String[] args) {
         int[] x = new int[]{1,3,6,4,1,2};
 
@@ -35,6 +60,7 @@ public class SimpleJavaProgram {
             System.out.println(x[i]);
 
         }
-
+        System.out.println("Smallest non occuring");
+        System.out.println(findLeastNonOccuring(x));
     }
 }
