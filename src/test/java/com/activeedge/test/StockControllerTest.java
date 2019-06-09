dce221027ace900585a5fb2a52f731d425ea7a7e
davidenoma <davidenoma@gmail.com>
2019-06-09 18:14:54 +0100

    Final commit

---
+++ src/test/java/com/activeedge/test/StockControllerTest.java
@@ -0,0 +1,24 @@
+package com.activeedge.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StockControllerTest {
+
+    @Test
+    public void createStock() {
+    }
+
+    @Test
+    public void updateStocks() {
+    }
+
+    @Test
+    public void getAllStocks() {
+    }
+
+    @Test
+    public void getStock() {
+    }
+}
